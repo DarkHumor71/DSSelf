06bbb8a234b1dc40d1b97995280e1e770deb9848
DarkHumor71 <128751050+DarkHumor71@users.noreply.github.com>
2024-02-27 12:06:15 +0200

    Update mtrxMultiply.java

--- src/graph/Matrix/mtrxMultiply.java
+++ src/graph/Matrix/mtrxMultiply.java
@@ -1,4 +1,15 @@
 package Matrix;
 public class mtrxMultiply{
-
+double [] [] multiply ( double [ ] [ ] a, double [ ] [ ] b ) {
+int m= a. length ;
+int n = a [ O ]. length
+int p= b. length
+int q = b [ O ]. length
+if ( n != p )  return null ; 
+double C [ ] [ ] = new double [ m ] [ q ]
+for (int i= O ; i < m ;i+ + ) {
+for (int j= O ; i < q ;j+ + ){
+for (int k= O ; k < n ; k+ + ){
+C [ i ] [ j ] + = a [ i ] [ k ] * b [ k ] [ j ] }}}
+return C ; }
 }
